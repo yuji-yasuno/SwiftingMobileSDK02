0494c47939cbd52a7296edd263ef7e53004b10c1
Yuji Yasuno <yuji.yasuno@gmial.com>
2015-03-12 17:37:56 +0900

    Initial commit

---
+++ UserDetail/UserDetail/UserDetail-Bridging-Header.h
@@ -0,0 +1,26 @@
+//
+//  UserDetail-Bridging-Header.h
+//  UserDetail
+//
+//  Created by 楊野 勇智 on 2015/03/12.
+//  Copyright (c) 2015年 salesforce.com. All rights reserved.
+//
+
+#ifndef UserDetail_UserDetail_Bridging_Header_h
+#define UserDetail_UserDetail_Bridging_Header_h
+
+#import <SalesforceRestAPI/SFRestAPI.h>
+#import <SalesforceRestAPI/SFRestAPI+Blocks.h>
+#import <SalesforceRestAPI/SFRestAPI+Files.h>
+#import <SalesforceRestAPI/SFRestAPI+QueryBuilder.h>
+#import <SalesforceRestAPI/SFRestRequest.h>
+#import <SalesforceOAuth/SFOAuthCoordinator.h>
+#import <SalesforceOAuth/SFOAuthCredentials.h>
+#import <SalesforceSDKCore/SalesforceSDKManager.h>
+#import <SalesforceSDKCore/SFDefaultUserManagementViewController.h>
+#import <SalesforceSDKCore/SFAuthenticationManager.h>
+#import <SalesforceSDKCore/SFUserAccountManager.h>
+#import <SalesforceSDKCore/SFJsonUtils.h>
+#import <SalesforceCommonUtils/SFLogger.h>
+
+#endif
---
+++ UserDetail/UserDetail/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  UserDetail
+//
+//  Created by 楊野 勇智 on 2015/03/12.
+//  Copyright (c) 2015年 salesforce.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var fullName: UILabel!
+    @IBOutlet weak var organization: UILabel!
+    @IBOutlet weak var mail: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let user = SFUserAccountManager.sharedInstance().currentUser
+        self.fullName.text = user.fullName
+        self.organization.text = user.organizationName
+        self.mail.text = user.email
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
